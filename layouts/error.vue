655e8bdc416febddd80a096c74acfc6827fb3fd0
Alex <zaychenko96y@outlook.com>
2018-08-09 22:53:35 +0300

    Added error page

---
+++ layouts/error.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-layout row wrap>
+    <v-flex xs12 class="text-xs-center" mt-3>
+      <h1>Ошибка 404</h1>
+    </v-flex>
+    <v-flex xs12 class="text-xs-center" mt-3>
+      <h2>Страница не найдена.</h2>
+    </v-flex>
+  </v-layout>
+</template>
