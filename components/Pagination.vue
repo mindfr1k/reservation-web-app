efc71b1f8c97642e83358537e38449f83f1b9e07
Alex <zaychenko96y@outlook.com>
2018-10-09 19:04:20 +0300

    Added basic pagination

---
+++ components/Pagination.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ul class="pagination">
+      <li class="disabled">
+        <nuxt-link to="1"><i class="material-icons">chevron_left</i></nuxt-link>
+      </li>
+      <li class="active"><nuxt-link to="animals/1">1</nuxt-link></li>
+      <li class="waves-effect"><nuxt-link to="animals/2">2</nuxt-link></li>
+      <li class="waves-effect">
+        <nuxt-link to="/1"><i class="material-icons">chevron_right</i></nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+  li.active a {
+    background-color: #ffd17c;
+  }
+</style>
--- pages/animals/_pageIndex/index.vue
+++ pages/animals/_pageIndex/index.vue
@@ -1,19 +1,25 @@
 <template>
   <div>
     <div class="row">
-      <div class="col s12 m6 l4" v-for="n in 9" :key="n">
+      <div class="col s12 m6 l4" v-for="i in 9" :key="i">
         <AnimalCard />
       </div>
     </div>
+    <div class="center-align">
+      <Pagination />
+    </div>
+
   </div>
 </template>
 
 <script>
-import AnimalCard from '@/components/AnimalCard';
+import AnimalCard from '@/components/AnimalCard'
+import Pagination from '@/components/Pagination'
 
 export default {
   components: {
-    AnimalCard
+    AnimalCard,
+    Pagination
   }
 }
 </script>
