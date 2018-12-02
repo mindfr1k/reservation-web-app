6a25d2c8fbc460ce06b34b94a8fef49139ad76c3
Alex <zaychenko96y@outlook.com>
2018-12-02 02:45:49 +0200

    Added frontend errors handling page

---
+++ layouts/error.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="center-align">
+    <div v-if="error.statusCode === 404">
+      <h1>Помилка 404</h1>
+      <h2>На жаль, такої сторінки не існує. Скористайтеся нашим меню :)</h2>
+      </div>
+    <h1 v-else>На сайті виникла помилка, просимо вибачення за незручності!</h1>
+    <nuxt-link to="/">До головної сторінки</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'error'
+  ]
+}
+</script>
