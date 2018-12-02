f8a508409ce0ea4aaaffe0347fceac2154d0be03
Alex <zaychenko96y@outlook.com>
2018-12-02 14:45:18 +0200

    Fixed pagination links

--- components/Pagination.vue
+++ components/Pagination.vue
@@ -3,13 +3,13 @@
     <div class="center-align" v-if="pages <= 6">
       <ul class="pagination">
         <li v-bind:class="getLeftChevronClass">
-          <nuxt-link :to="`/animals/${previousPage}`"><i class="material-icons">chevron_left</i></nuxt-link>
+          <nuxt-link :to="`/${currentCategory}/${previousPage}`"><i class="material-icons">chevron_left</i></nuxt-link>
         </li>
         <li v-for="page in pages" :key="page" v-bind:class="getPageClass(page)">
-          <nuxt-link :to="`/animals/${page}`">{{ page }}</nuxt-link>
+          <nuxt-link :to="`/${currentCategory}/${page}`">{{ page }}</nuxt-link>
         </li>
         <li v-bind:class="getRightChevronClass">
-          <nuxt-link :to="`/animals/${nextPage}`"><i class="material-icons">chevron_right</i></nuxt-link>
+          <nuxt-link :to="`/${currentCategory}/${nextPage}`"><i class="material-icons">chevron_right</i></nuxt-link>
         </li>
       </ul>
     </div>
@@ -17,70 +17,70 @@
     <div class="center-align" v-if="pages > 6">
       <ul class="pagination" v-if="currentPage <= 2 || currentPage > pages - 2">
         <li v-bind:class="getLeftChevronClass">
-          <nuxt-link :to="`/animals/${previousPage}`"><i class="material-icons">chevron_left</i></nuxt-link>
+          <nuxt-link :to="`/${currentCategory}/${previousPage}`"><i class="material-icons">chevron_left</i></nuxt-link>
         </li>
         <li v-for="page in pages" :key="page" v-bind:class="getPageClass(page)" v-if="page <= 3">
-          <nuxt-link :to="`/animals/${page}`">{{ page }}</nuxt-link>
+          <nuxt-link :to="`/${currentCategory}/${page}`">{{ page }}</nuxt-link>
         </li>
         <span>...</span>
         <li v-for="page in pages" :key="page" v-bind:class="getPageClass(page)" v-if="page >= pages - 2">
-          <nuxt-link :to="`/animals/${page}`">{{ page }}</nuxt-link>
+          <nuxt-link :to="`/${currentCategory}/${page}`">{{ page }}</nuxt-link>
         </li>
         <li v-bind:class="getRightChevronClass">
-          <nuxt-link :to="`/animals/${nextPage}`"><i class="material-icons">chevron_right</i></nuxt-link>
+          <nuxt-link :to="`/${currentCategory}/${nextPage}`"><i class="material-icons">chevron_right</i></nuxt-link>
         </li>
       </ul>
 
       <ul class="pagination" v-else-if="currentPage === 3">
         <li v-bind:class="getLeftChevronClass">
-          <nuxt-link :to="`/animals/${previousPage}`"><i class="material-icons">chevron_left</i></nuxt-link>
+          <nuxt-link :to="`/${currentCategory}/${previousPage}`"><i class="material-icons">chevron_left</i></nuxt-link>
         </li>
         <li v-for="page in pages" :key="page" v-bind:class="getPageClass(page)" v-if="page <= 5">
-          <nuxt-link :to="`/animals/${page}`">{{ page }}</nuxt-link>
+          <nuxt-link :to="`/${currentCategory}/${page}`">{{ page }}</nuxt-link>
         </li>
         <span>...</span>
         <li v-bind:class="getPageClass(page)">
-          <nuxt-link :to="`/animals/${pages}`">{{ pages }}</nuxt-link>
+          <nuxt-link :to="`/${currentCategory}/${pages}`">{{ pages }}</nuxt-link>
         </li>
         <li v-bind:class="getRightChevronClass">
-          <nuxt-link :to="`/animals/${nextPage}`"><i class="material-icons">chevron_right</i></nuxt-link>
+          <nuxt-link :to="`/${currentCategory}/${nextPage}`"><i class="material-icons">chevron_right</i></nuxt-link>
         </li>
       </ul>
 
       <ul class="pagination" v-else-if="currentPage === pages - 2">
         <li v-bind:class="getLeftChevronClass">
-          <nuxt-link :to="`/animals/${previousPage}`"><i class="material-icons">chevron_left</i></nuxt-link>
+          <nuxt-link :to="`/${currentCategory}/${previousPage}`"><i class="material-icons">chevron_left</i></nuxt-link>
         </li>
         <li v-bind:class="getPageClass(page)">
-          <nuxt-link :to="`/animals/1`">1</nuxt-link>
+          <nuxt-link :to="`/${currentCategory}/1`">1</nuxt-link>
         </li>
         <span>...</span>
         <li v-for="page in pages" :key="page" v-bind:class="getPageClass(page)" v-if="page >= pages - 4">
-          <nuxt-link :to="`/animals/${page}`">{{ page }}</nuxt-link>
+          <nuxt-link :to="`/${currentCategory}/${page}`">{{ page }}</nuxt-link>
         </li>
         <li v-bind:class="getRightChevronClass">
-          <nuxt-link :to="`/animals/${nextPage}`"><i class="material-icons">chevron_right</i></nuxt-link>
+          <nuxt-link :to="`/${currentCategory}/${nextPage}`"><i class="material-icons">chevron_right</i></nuxt-link>
         </li>
       </ul>
 
       <ul class="pagination" v-else>
         <li v-bind:class="getLeftChevronClass">
-          <nuxt-link :to="`/animals/${previousPage}`"><i class="material-icons">chevron_left</i></nuxt-link>
+          <nuxt-link :to="`/${currentCategory}/${previousPage}`"><i class="material-icons">chevron_left</i></nuxt-link>
         </li>
         <li v-bind:class="getPageClass(page)">
-          <nuxt-link :to="`/animals/1`">1</nuxt-link>
+          <nuxt-link :to="`/${currentCategory}/1`">1</nuxt-link>
         </li>
         <span>...</span>
         <li v-for="page in pages" :key="page" v-bind:class="getPageClass(page)" 
         v-if="page === currentPage || page === currentPage - 1 || page === currentPage + 1">
-          <nuxt-link :to="`/animals/${page}`">{{ page }}</nuxt-link>
+          <nuxt-link :to="`/${currentCategory}/${page}`">{{ page }}</nuxt-link>
         </li>
         <span>...</span>
         <li v-bind:class="getPageClass(page)">
-          <nuxt-link :to="`/animals/${pages}`">{{ pages }}</nuxt-link>
+          <nuxt-link :to="`/${currentCategory}/${pages}`">{{ pages }}</nuxt-link>
         </li>
         <li v-bind:class="getRightChevronClass">
-          <nuxt-link :to="`/animals/${nextPage}`"><i class="material-icons">chevron_right</i></nuxt-link>
+          <nuxt-link :to="`/${currentCategory}/${nextPage}`"><i class="material-icons">chevron_right</i></nuxt-link>
         </li>
       </ul>
 
@@ -93,6 +93,9 @@ export default {
   computed: {
     pages() {
       return this.$store.state.categoryPages;
+    },
+    currentCategory() {
+      return this.$store.state.currentCategory;
     },
     currentPage() {
       return parseInt(this.$route.params.pageIndex);
